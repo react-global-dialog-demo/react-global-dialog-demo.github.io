f678487c69038c5b1d6e27790b28e24bb57cb82e
lisper <leyapin@gmail.com>
2019-02-26 01:20:08 +0800

    init

---
+++ precache-manifest.59b51e6568044b5039e105c9f4fce16c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "709548bfef5ddb4b500f",
+    "url": "/static/js/main.709548bf.chunk.js"
+  },
+  {
+    "revision": "6d5f2a97d99686458a25",
+    "url": "/static/js/1.6d5f2a97.chunk.js"
+  },
+  {
+    "revision": "709548bfef5ddb4b500f",
+    "url": "/static/css/main.cd6703fb.chunk.css"
+  },
+  {
+    "revision": "b3425a1aeffae3196f742e274983a937",
+    "url": "/index.html"
+  }
+];
